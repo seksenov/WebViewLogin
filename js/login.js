b23abffbd0ef95c6aa465be9605ade92273f7e8f
unknown <kiseksen@MININT-FKBKPRG.redmond.corp.microsoft.com>
2014-10-14 10:21:32 -0700

    added FB integration and loaded the FB SDK

--- js/login.js
+++ js/login.js
@@ -4,6 +4,24 @@ if (window.location.protocol != "https:") {
    window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
 }
 
+//Setup the Facebook SDK
+window.fbAsyncInit = function() {
+	FB.init({
+	  appId      : '748913388496088',
+	  xfbml      : true,
+	  version    : 'v2.1'
+	});
+};
+
+(function(d, s, id){
+ var js, fjs = d.getElementsByTagName(s)[0];
+ if (d.getElementById(id)) {return;}
+ js = d.createElement(s); js.id = id;
+ js.src = "//connect.facebook.net/en_US/sdk.js";
+ fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+ //DO the manual FB login
 function loginFB() {
     console.log("Starting Facebook login");
 }
