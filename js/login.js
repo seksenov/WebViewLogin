fc2ffed7d8c671c5543cd0fd3bda097d7a87d22d
unknown <kiseksen@MININT-FKBKPRG.redmond.corp.microsoft.com>
2014-10-14 10:14:46 -0700

    forcing into https://

--- js/login.js
+++ js/login.js
@@ -1,3 +1,9 @@
+//Force into https://
+if (window.location.protocol != "https:") {
+//  console.log("Forced the page to load into https://");
+   window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+}
+
 function loginFB() {
     console.log("Starting Facebook login");
 }
